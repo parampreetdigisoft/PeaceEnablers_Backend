34a3397612c3ba30fd1511f0a930d473afd7bea7
Param <parampreet.digisoft@gmail.com>
2026-03-12 15:14:25 +0530

    sdfs

--- PeaceEnablers/Common/DI/ServiceRegistration.cs
+++ PeaceEnablers/Common/DI/ServiceRegistration.cs
@@ -35,6 +35,7 @@ namespace PeaceEnablers.Common.DI
             services.AddScoped<IKpiService, KpiService>();
             services.AddScoped<IAIComputationService, AIComputationService>();
             services.AddScoped<ICommonService, CommonService>();
+            services.AddScoped<IPdfGeneratorService, PdfGeneratorService>();
             return services;
         }
     }
